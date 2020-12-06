e016a683fe4eab20130e255185e18a8e85d3ae09
StrongGarl1c <jarkkocs16@rambler.ru>
2020-12-06 14:59:32 +0200

    fetch users names

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,14 +1,42 @@
-async function Employees() {
-  const data = await fetch(
-    'https://yalantis-react-school-api.yalantis.com/api/task0/users',
-  );
+import React, { useState, useEffect } from 'react';
 
-  return (
-    <div>
-      <h1>test</h1>
-      {data}
-    </div>
-  );
+function Employees() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        },
+      );
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.firstName} {item.lastName}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default Employees;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Employees from './components/Employees';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Employees />
   </React.StrictMode>,
   document.getElementById('root'),
 );
